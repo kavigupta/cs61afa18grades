8d938316cafeab5776e52c06ad9bd180e896425a
Kavi Gupta <kavi@globalmbox.com>
2018-12-06 14:31:58 -0800

    Make bookmarklet correspond to most recent code

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://cdn.jsdelivr.net/gh/kavigupta/cs61afa18grades/fa18grades.js';})();
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://cdn.jsdelivr.net/gh/kavigupta/cs61afa18grades@latest/fa18grades.js';})();
